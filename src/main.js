4e5f8ca047f19a7dd8883ea5fb4c1b7679af04e6
hessnd <n.hess@bigspaceship.com>
2019-02-08 21:30:30 -0500

    dark mode

--- src/main.js
+++ src/main.js
@@ -9,4 +9,43 @@ const setCopyright = () => {
 
 window.onload = () => {
   setCopyright();
+
+  const state = {
+    isDark: false,
+    darkMode: {
+      background: 'black',
+      text: 'white',
+    },
+    lightMode: {
+      background: 'white',
+      text: 'black',
+    },
+  };
+
+  // get toggle button
+  const toggle = document.getElementById('dark-mode-toggle');
+  const myName = document.getElementById('my-name');
+
+  const setColor = ({ background, text }) => {
+    document.body.style.backgroundColor = background;
+    document.body.style.color = text;
+    myName.style.borderBottom = `solid ${text} 4px`;
+  };
+
+  const toggleColor = () => {
+    if (state.isDark) {
+      setColor(state.darkMode);
+    } else if (!state.isDark) {
+      setColor(state.lightMode);
+    }
+  };
+
+  // initialize button to initial state
+  toggle.checked = state.isDark;
+  toggleColor();
+
+  toggle.addEventListener('change', () => {
+    state.isDark = !state.isDark;
+    toggleColor();
+  });
 };
