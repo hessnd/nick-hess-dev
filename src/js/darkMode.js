a4c97939e78d719e029e2a4da9a48505e7611e86
hessnd <n.hess@bigspaceship.com>
2019-02-12 19:57:45 -0500

    move dark mode functions into own file

--- src/js/darkMode.js
+++ src/js/darkMode.js
@@ -1,40 +1,78 @@
-export default class DarkMode {
-  constructor(document) {
-    this.state = {
-      isDark: false,
-      darkMode: {
-        background: 'black',
-        text: 'white',
-      },
-      lightMode: {
-        background: 'white',
-        text: 'black',
-      },
-    };
-    this.document = document;
-    // get toggle button
-    this.toggle = document.getElementById('dark-mode-toggle');
-    this.myName = document.getElementById('my-name');
-    console.log(this.document);
-  }
+'use-strict';
 
-  setState({ key, val }) {
-    this.state[key] = val;
-  }
+// typography colors
+const slate = '#393f42';
+const whitesmoke = '#f5f5f5';
 
-  setColor({ background, text }) {
-    console.log('set color');
-    this.document.body.style.backgroundColor = background;
-    this.document.body.style.color = text;
-    this.myName.style.borderBottom = `solid ${text} 4px`;
-  }
+// color scheme state
+const state = {
+  isDark: false,
+  darkMode: {
+    background: slate,
+    headers: whitesmoke,
+    body: 'white',
+  },
+  lightMode: {
+    background: 'white',
+    headers: slate,
+    body: 'black',
+  },
+};
 
-  toggleColor() {
-    this.setState({ isDark: !this.state.isDark });
-    if (this.state.isDark) {
-      this.setColor(this.state.darkMode);
-    } else if (!this.state.isDark) {
-      this.setColor(this.state.lightMode);
+// get toggle button
+const toggle = document.getElementById('dark-mode-toggle');
+const safariColorScheme = window.matchMedia('(prefers-color-scheme: dark)');
+
+const setColor = ({ background, headers, body }) => {
+  // set background color
+  document.body.style.backgroundColor = background;
+
+  // get all elements with class .body and apply styles
+  const bodyElements = document.querySelectorAll('.body');
+  bodyElements.forEach(elem => {
+    elem.style.color = body;
+  });
+
+  // get all elements with class .header and apply styles
+  const headerElements = document.querySelectorAll('.header');
+  headerElements.forEach(elem => {
+    elem.style.color = headers;
+    if (elem.className.includes('border-bottom')) {
+      elem.style.borderBottom = `solid ${headers} 4px`;
     }
+  });
+};
+
+const toggleColor = () => {
+  if (state.isDark) {
+    setColor(state.darkMode);
+  } else if (!state.isDark) {
+    setColor(state.lightMode);
   }
+};
+
+// set dark mode for safari native color scheme
+const setDark = e => {
+  state.isDark = e.matches;
+  toggleColor();
+};
+
+export default function initDarkModeHelper() {
+  // listener for safari color scheme
+  safariColorScheme.addListener(setDark);
+
+  // initialize color scheme to initial state
+  setDark(safariColorScheme);
+  toggle.checked = state.isDark;
+  toggleColor();
+
+  // add event listener
+  toggle.addEventListener('change', () => {
+    state.isDark = !state.isDark;
+    toggleColor();
+  });
 }
+
+
+
+
--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 'use-strict';
+import initDarkModeHelper from './js/darkMode';
 import './styles.scss';
 
 const setCopyright = () => {
@@ -8,75 +9,10 @@ const setCopyright = () => {
   copy.innerHTML = `Copyright &copy; ${year} Nick Hess`;
 };
 
-// typography colors
-const slate = '#393f42';
-const whitesmoke = '#f5f5f5';
-
-// color scheme state
-const state = {
-  isDark: false,
-  darkMode: {
-    background: slate,
-    headers: whitesmoke,
-    body: 'white',
-  },
-  lightMode: {
-    background: 'white',
-    headers: slate,
-    body: 'black',
-  },
-};
-
 // set copyright
 setCopyright();
+initDarkModeHelper();
 
-// get toggle button
-const toggle = document.getElementById('dark-mode-toggle');
-const nativeColorScheme = window.matchMedia('(prefers-color-scheme: dark)');
 
-const setColor = ({ background, headers, body }) => {
-  // set background color
-  document.body.style.backgroundColor = background;
 
-  // get all elements with class .body and apply styles
-  const bodyElements = document.querySelectorAll('.body');
-  bodyElements.forEach(elem => {
-    elem.style.color = body;
-  });
 
-  // get all elements with class .header and apply styles
-  const headerElements = document.querySelectorAll('.header');
-  headerElements.forEach(elem => {
-    elem.style.color = headers;
-    if (elem.className.includes('border-bottom')) {
-      elem.style.borderBottom = `solid ${headers} 4px`;
-    }
-  });
-};
-
-const toggleColor = () => {
-  if (state.isDark) {
-    setColor(state.darkMode);
-  } else if (!state.isDark) {
-    setColor(state.lightMode);
-  }
-};
-
-// set dark mode for safari native color scheme
-const setDark = e => {
-  state.isDark = e.matches;
-  toggleColor();
-};
-
-nativeColorScheme.addListener(setDark);
-
-// initialize color scheme to initial state
-setDark(nativeColorScheme);
-toggle.checked = state.isDark;
-toggleColor();
-
-// add event listener
-toggle.addEventListener('change', () => {
-  state.isDark = !state.isDark;
-  toggleColor();
-});
