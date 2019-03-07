34eb451cdb387ae3821bcd97ba66b2feae0aea73
hessnd <n.hess@bigspaceship.com>
2019-03-07 12:00:35 -0500

    working on updating service worker

--- src/service-worker.js
+++ src/service-worker.js
@@ -2,10 +2,21 @@ self.addEventListener('install', event => {
   event.waitUntil(
     caches.open('hesstopher-v1').then(cache => {
       return cache.addAll([
-        '/js/**.*',
-        '/fonts/**.*',
-        '/styles.css',
+        '*.{html,woff,woff2,ttf}',
+        'main.1f19ae8e.js',
+        'main.1f19ae8e.css',
       ]);
     })
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => Promise.all(
+      cacheNames.filter(cacheName => {
+        console.log(cacheName);
+        return true;
+      }).map(cacheName => caches.delete(cacheName))
+    ))
   );
 });
