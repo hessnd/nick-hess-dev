a493a30c7dbc079acfdc5f4a9b56a719bc56f4be
hessnd <nick@nickhess.dev>
2021-12-08 17:49:52 -0700

    get contentful api working

--- lib/api.ts
+++ lib/api.ts
@@ -1,39 +1,44 @@
-const EMPLOYER_GRAPHQL_FEILDS = `
-  name
-`
+const CONTENTFUL_SPACE_ID = process.env.CONTENTFUL_SPACE_ID || '';
+const CONTENTFUL_PREVIEW_ACCESS_TOKEN =
+  process.env.CONTENTFUL_PREVIEW_ACCESS_TOKEN || '';
+const CONTENTFUL_ACCESS_TOKEN = process.env.CONTENTFUL_ACCESS_TOKEN || '';
 
-async function fetchGraphQL(query: any, preview: boolean = false) {
+async function fetchGraphQL(query: string, preview = false) {
   return fetch(
-    `https://graphql.contentful.com/content/v1/spaces/${process.env.CONTENTFUL_SPACE_ID}`,
+    `https://graphql.contentful.com/content/v1/spaces/${CONTENTFUL_SPACE_ID}`,
     {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
         Authorization: `Bearer ${
-          preview
-            ? process.env.CONTENTFUL_PREVIEW_ACCESS_TOKEN
-            : process.env.CONTENTFUL_ACCESS_TOKEN
+          preview ? CONTENTFUL_PREVIEW_ACCESS_TOKEN : CONTENTFUL_ACCESS_TOKEN
         }`,
       },
       body: JSON.stringify({ query }),
     }
-  ).then((response) => response.json())
+  ).then((response) => response.json());
 }
 
 function extractEmployerEntries(fetchResponse: any) {
-  return fetchResponse?.data?.employerCollection?.items;
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+  return fetchResponse?.data?.jobCollection?.items;
 }
 
+// eslint-disable-next-line import/prefer-default-export
 export async function getEmployers(preview: boolean) {
-  const entries = await fetchGraphQL(
-    `query {
-      employerCollection(preview: ${preview ? 'true' : 'false'}) {
-        items {
-          ${EMPLOYER_GRAPHQL_FEILDS}
+  try {
+    const entries: any = await fetchGraphQL(
+      `query {
+        jobCollection(preview: ${preview ? 'true' : 'false'}) {
+          items {
+            name
+          }
         }
-      }
-    }`,
-    preview
-  )
-  return extractEmployerEntries(entries)
+      }`,
+      preview
+    );
+    return extractEmployerEntries(entries);
+  } catch (error) {
+    throw new Error('Could not fetch');
+  }
 }
